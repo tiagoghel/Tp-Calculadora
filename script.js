3b86ce67c59bf166bdbddee4d4885eb3deb28648
MAT�AS JOAQUIN CASTERA <48385887@ort>
2025-03-20 10:38:22 -0300

    asd

---
+++ script.js
@@ -0,0 +1,24 @@
+const resultado = document.getElementById("pantalla");
+
+const escribir = (num) => {
+    resultado.value += num;
+}
+
+ /*const cuenta = (num1, num2) => {
+    if(){
+    resultado = num1 + num2
+    }
+    if()
+    {
+
+    }
+    if()
+    {
+
+    }
+    if()
+    {
+
+    }
+} */
+
