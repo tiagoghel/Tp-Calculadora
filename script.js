53586db3e860bc44c59e5ec2cc636d96afe527a1
MAT�AS JOAQUIN CASTERA <48385887@ort>
2025-03-26 10:39:24 -0300

    asdd

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
 const resultado = document.getElementById("pantalla");
+let primerNumero = 0;
+let segundoNumero = 0;
+let operacion;
 
-const escribir = (num) => {
+const guardarNumero = (num) => {
+    if (operacion) {
+        segundoNumero += num;
+    } else {
+        primerNumero += num;
+    }
+    
     resultado.value += num;
 }
 
- /*const cuenta = (num1, num2) => {
-    if(){
-    resultado = num1 + num2
-    }
-    if()
+const guardarOperacion = (valor) => 
+{
+
+    switch(valor)
     {
-
+        case 1: operacion = '+'; break;
+        case 2: operacion = '-'; break;
+        case 3: operacion = '*'; break;
+        case 4:  operacion = '/'; break;
     }
-    if()
+    resultado.value += operacion;
+
+}
+
+const resolver = () => {
+    let resul = 0;
+    switch(operacion)
     {
-
+        case "+": 
+            resul = parseInt(primerNumero) + parseInt(segundoNumero); 
+            break;
+        case "-": 
+            resul = parseInt(primerNumero) - parseInt(segundoNumero);
+            break;
+        case "*": 
+            resul = parseInt(primerNumero) * parseInt(segundoNumero);
+            break;
+        case "/": 
+            resul = parseFloat(primerNumero) / parseFloat(segundoNumero);
+            break;
     }
-    if()
+    resultado.value = resul;
+    if(resul > 0)
     {
-
+        resultado.style.color = "Green";
     }
-} */
+    else {
+        resultado.style.color = "Red";
+    }
+        primerNumero = resul;
+        segundoNumero = 0;
+        operacion = "";
+} 
 
+const borrar = () => {
+    document.getElementById("pantalla").value= "";   
+}
